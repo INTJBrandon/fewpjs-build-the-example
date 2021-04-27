02a8b7b8de593eab79efd22550f727283fecd9ba
seireitou <ausis@mail.com>
2021-04-26 22:54:40 -0500

    commit tree 3be5b2ce7846d902166921d499c1c25637a7c416

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const like_glyph = document.querySelectorAll(".like-glyph");
+like_glyph.forEach((heart) => {
+  heart.addEventListener("click", e => {
+    mimicServerCall()
+    .then(obj => {
+      like(e);
+    })
+    .catch(error => {
+      const modal = document.querySelector("#modal");
+      const modal_message = document.querySelector("#modal-message");
+      modal_message.innerHTML = error;
+      modal.classList.remove("hidden");
+    });
+  });
+});
 
+function like(e) {
+  if (e.target.textContent == EMPTY_HEART) {
+    e.target.textContent = FULL_HEART;
+    e.target.classList.add("activated-heart");
+  } else {
+    e.target.textContent = EMPTY_HEART;
+    e.target.classList.remove("activated-heart");
+  }
+}
 
 
 
